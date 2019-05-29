b46eef128b9cdd0bd644537b7fbfcc2a9d9e71ee
Michail Khlebnikov <frontend.dev.ru@gmail.com>
2019-05-29 15:27:21 +0300

    Fixed

--- public/app-project.js
+++ public/app-project.js
@@ -2,14 +2,12 @@ var app = new Vue({
     el: '#app',
     data: {},
     computed: {},
-    methods: {
-        // initSelect2: function () {
-            // $('.configuration__select').select2();
-        // }
-    },
+    methods: {},
     mounted() {
         $('.configuration__select_long').select2({
-            minimumResultsForSearch: -1
+            minimumResultsForSearch: -1,
+        }).on("select2:open", function () {
+            $('.select2-results__options').perfectScrollbar();
         });
         $('.configuration__select_shot').select2({
             width: '105px',
